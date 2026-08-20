87f4f13bade348c5ea28b749b1cb9ebc47a29cf9
jandui jr <jandui500@gmail.com>
2026-08-20 14:30:43 -0300

    versao 1.1

--- __main__.py
+++ __main__.py
@@ -7,42 +7,88 @@ class No():
 class Lista():
     def __init__(self):
         self.cabeca = None
+        self.tamanho = 0
     
     def inserir(self,valor1,valor2):
         novo = No(valor1,valor2)
         if self.cabeca is None:
             self.cabeca = novo
+            self.tamanho +=1
             return
         atual = self.cabeca
         while atual.proximo is not None:
             atual = atual.proximo
     
         atual.proximo = novo
+        self.tamanho += 1
     
     def exibir_all(self):
         atual = self.cabeca
         
-        if atual == None:
+        if atual is None:
             print('A lista esta vazia!')
             return
         
+        print('Exibindo todos os valores:')
         while atual is not None:
             print(f'Valor 1: {atual.valor1}\nValor 2: {atual.valor2}')
             atual = atual.proximo
     
-    def Obter_Proximo(self, no):
-        if self.cabeca is None:
-            print('A lista esta vazia!')
+    def obter_Proximo(self, no):
+        if no is None:
+            print('No informado é invalido!')
+            return
+        if no.proximo is None:
+            print('Nao ha proximo!')
             return
         print('Proximos valores:')
         print(f'Valor 1: {no.proximo.valor1}\nValor 2: {no.proximo.valor2}')
     
-    def Obter_Valor(self, no):
-        if self.cabeca is None:
-            print('A lista esta vazia!')
+    def obter_Valor(self, no):
+        if no is None:
+            print('O No nao existe!')
             return
         print('Valores do No:')
         print(f'Valor 1: {no.valor1}\nValor 2: {no.valor2}')
+    
+    def alterar_no(self,no,valor1,valor2):
+        if no is None:
+            print('Nao existe esse No!')
+            return
+        no.valor1 = valor1
+        no.valor2 = valor2
+        print('VALORES ALTERADOS: ')
+        print(f'Valor 1: {no.valor1}\nValor 2: {no.valor2}')
+    
+    def exibir_tamanho(self):
+        print(f'A lista possui tamanho {self.tamanho}')
+        
+    def existe(self,no):
+        tem = False
+        atual = self.cabeca
+        while atual is not None:
+            if atual is no:
+                tem = True
+            atual = atual.proximo
+        if tem == True:
+            print('Existe!')
+        else:
+            print('Nao existe!')
+    
+    def buscar(self, valor):
+        busca = False
+        atual = self.cabeca
+        while atual is not None:
+            if valor == atual.valor1 or valor == atual.valor2:
+                busca = True
+            atual = atual.proximo
+        if busca == True:
+            print('Existe valor!')
+        else:
+            print('Nao existe valor!')
+
+        
+         
     
         
         
@@ -52,10 +98,24 @@ lista1 = Lista()
 lista1.inserir(3,4)
 lista1.inserir(1,2)
 lista1.inserir(8,9)
-lista1.exibir_all()
+
+
+#NOS:
+primeiro = lista1.cabeca
 segundo = lista1.cabeca.proximo
-lista1.Obter_Proximo(segundo)
-lista1.Obter_Valor(segundo)
+terceiro = segundo.proximo
+quarto = terceiro.proximo
+
+lista1.existe(quarto)
+lista1.obter_Valor(segundo)
+lista1.obter_Proximo(terceiro)
+lista1.alterar_no(segundo,6,7)
+lista1.exibir_all()
+lista1.exibir_tamanho()
+lista1.buscar(8)
+
+
+
 
 
 
