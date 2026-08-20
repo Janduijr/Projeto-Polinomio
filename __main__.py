467cce17d3a428bd87ae777a75da5cc3abcabb96
Jandui Junior <134757940+Janduijr@users.noreply.github.com>
2026-08-20 00:33:43 -0300

    Versao 1.0

--- __main__.py
+++ __main__.py
@@ -29,6 +29,21 @@ class Lista():
         while atual is not None:
             print(f'Valor 1: {atual.valor1}\nValor 2: {atual.valor2}')
             atual = atual.proximo
+    
+    def Obter_Proximo(self, no):
+        if self.cabeca is None:
+            print('A lista esta vazia!')
+            return
+        print('Proximos valores:')
+        print(f'Valor 1: {no.proximo.valor1}\nValor 2: {no.proximo.valor2}')
+    
+    def Obter_Valor(self, no):
+        if self.cabeca is None:
+            print('A lista esta vazia!')
+            return
+        print('Valores do No:')
+        print(f'Valor 1: {no.valor1}\nValor 2: {no.valor2}')
+    
         
         
 
@@ -36,8 +51,11 @@ class Lista():
 lista1 = Lista()
 lista1.inserir(3,4)
 lista1.inserir(1,2)
+lista1.inserir(8,9)
 lista1.exibir_all()
-
+segundo = lista1.cabeca.proximo
+lista1.Obter_Proximo(segundo)
+lista1.Obter_Valor(segundo)
 
 
 
